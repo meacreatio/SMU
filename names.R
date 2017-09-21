f15e338e014c06cbc0911e9f9f91fe8c623e792c
Bruce Granger <brucegranger@gmail.com>
2017-09-20 21:42:39 -0500

    Bruce's Update of the names.r file

--- names.R
+++ names.R
@@ -1,6 +1,6 @@
 # Names.R
 # Created by [GROUP NAME HERE]
-# Date: [INSERT DATE HERE]
+# Date: 9-20-2017
 # Tell us what your name is!
 
 # Original Repo-Owner, please write in here
@@ -10,7 +10,7 @@ print("Hello, my name is Matt Pavlovich")
 # Don't do this *for* other people.  Just let them pull the repo, add a line for their own name, commit, and push it!
 # Remember not to do this all at once.  Have one group member do it at a time to prevent conflicts.
 
-print("")
+print("Bruce Granger")
 
 print("")
 
